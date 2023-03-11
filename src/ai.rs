d617191c825083532d0312b218b888a865a80cdd
nada <37610161+furoxr@users.noreply.github.com>
2023-03-11 15:07:23 +0800

    feat:Refactor OpenAI API interactions for better token management.
    [src/ai.rs]
    - Add two structs, `TokenEncoder` and `Openai`, to handle token encoding and management of the OpenAI API
    - Move the `num_tokens_from_messages` function to the `TokenEncoder` struct
    - Change the argument of `num_tokens_from_messages` function from `&ConversationCtx` to `&VecDeque<ChatCompletionRequestMessage>`
    - Add a new function in `TokenEncoder` to get the number of tokens from a single message
    - Modify `num_tokens_from_messages` function to call the new function for every message in the vector
    - Add a new function in `Openai` struct called `shrink_conversation` that shortens a conversation in the `ConversationCtx` struct given a limit of tokens
    - Add tests for the `num_tokens_from_messages` and `shrink_conversation` functions in the `tests` module.

--- src/ai.rs
+++ src/ai.rs
@@ -1,11 +1,17 @@
-use anyhow::Result;
-use tiktoken_rs::tiktoken::cl100k_base;
+use std::collections::VecDeque;
+
+use anyhow::{anyhow, Result};
+use async_openai::{types::ChatCompletionRequestMessage, Client};
+use tiktoken_rs::tiktoken::{cl100k_base, CoreBPE};
 
 use crate::conversation::ConversationCtx;
 
 /// Calculate tokens consumed in the chat api of openai. Check the calculation algorithm here:
 /// https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
-fn num_tokens_from_messages(messages: &ConversationCtx, model: &str) -> Result<usize> {
+pub fn num_tokens_from_messages(
+    messages: &VecDeque<ChatCompletionRequestMessage>,
+    model: &str,
+) -> Result<usize> {
     let bpe = cl100k_base()?;
     let mut num_tokens = 0;
     if model == "gpt-3.5-turbo-0301" {
@@ -23,18 +29,98 @@ fn num_tokens_from_messages(messages: &ConversationCtx, model: &str) -> Result<u
     Ok(num_tokens)
 }
 
+pub struct TokenEncoder(pub CoreBPE);
+
+impl TokenEncoder {
+    pub fn new() -> Result<Self> {
+        Ok(Self(cl100k_base()?))
+    }
+
+    pub fn num_tokens_from_message(&self, message: &ChatCompletionRequestMessage) -> Result<usize> {
+        let mut num_tokens = 0;
+        num_tokens += self.0.encode_with_special_tokens(&message.content).len();
+        num_tokens += self
+            .0
+            .encode_with_special_tokens(&message.role.to_string())
+            .len();
+        if let Some(name) = &message.name {
+            num_tokens += self.0.encode_with_special_tokens(name).len() - 1;
+        }
+
+        Ok(num_tokens)
+    }
+
+    pub fn num_tokens_from_messages(
+        &self,
+        messages: &VecDeque<ChatCompletionRequestMessage>,
+    ) -> Result<usize> {
+        let mut num_tokens = 0;
+        for msg in messages.iter() {
+            num_tokens += 4;
+            num_tokens += self.num_tokens_from_message(msg)?;
+        }
+        num_tokens += 2;
+
+        Ok(num_tokens)
+    }
+}
+
+pub struct Openai(pub Client, pub TokenEncoder);
+
+impl Openai {
+    pub fn new(api_key: &str) -> Result<Self> {
+        Ok(Self(
+            Client::new().with_api_key(api_key),
+            TokenEncoder::new()?,
+        ))
+    }
+
+    pub fn shrink_conversation<'a>(
+        &'a self,
+        ctx: &'a mut ConversationCtx,
+        limit: usize,
+    ) -> Result<&mut ConversationCtx> {
+        let mut messages_count = VecDeque::with_capacity(ctx.value.len());
+        let mut tokens: usize = 0;
+        for msg in ctx.value.iter() {
+            tokens += 4;
+            let num_tokens = self.1.num_tokens_from_message(msg)?;
+            tokens += num_tokens;
+            messages_count.push_back(num_tokens);
+        }
+        tokens += 2;
+
+        if tokens <= limit {
+            return Ok(ctx);
+        } else if tokens > limit && messages_count.len() <= 2 {
+            return Err(anyhow!("Conversation is too short to shrink."));
+        } else {
+            messages_count.pop_front();
+            let mut i = 0;
+            while tokens > limit && messages_count.len() > 1 {
+                tokens -= messages_count[0];
+                tokens -= 4;
+                messages_count.pop_front();
+                i += 1;
+            }
+
+            if tokens > limit {
+                return Err(anyhow!("Conversation can not be shrinked. "));
+            }
+            ctx.value.drain(1..=i);
+        }
+
+        Ok(ctx)
+    }
+}
+
 // test mod
 #[cfg(test)]
 mod tests {
-    use async_openai::types::Role;
+    use super::Openai;
+    use crate::conversation::ConversationCtx;
 
-    #[test]
-    fn test_token_calculation() {
-        use crate::conversation::ConversationCtx;
-
-        use super::num_tokens_from_messages;
-
-        println!("{}", Role::System.to_string());
+    fn data() -> ConversationCtx {
         let mut ctx = ConversationCtx::default();
         ctx.add_system_message("You are a helpful, pattern-following assistant that translates corporate jargon into plain English.", None)
         .add_system_message("New synergies will help drive top-line growth.", Some("example_user".into()))
@@ -42,7 +128,41 @@ mod tests {
         .add_system_message("Let's circle back when we have more bandwidth to touch base on opportunities for increased leverage.", Some("example_user".into()))
         .add_system_message("Let's talk later when we're less busy about how to do better.", Some("example_assistant".into()))
         .add_user_message("This late pivot means we don't have time to boil the ocean for the client deliverable.", None);
-        let nums = num_tokens_from_messages(&ctx, "gpt-3.5-turbo-0301").unwrap();
+        ctx
+    }
+
+    #[test]
+    fn test_token_calculation() {
+        let ctx = data();
+        let ai = Openai::new("test").unwrap();
+        let nums = ai.1.num_tokens_from_messages(&ctx.value).unwrap();
         assert_eq!(nums, 126);
+    }
+
+    #[test]
+    fn test_shrink_conversation() {
+        let ai = Openai::new("test").unwrap();
+        let mut ctx = data();
+        let result = ai.shrink_conversation(&mut ctx, 125);
+        assert!(result.is_ok());
+
+        let mut ctx = data();
+        let result = ai.shrink_conversation(&mut ctx, 49);
+        assert!(result.is_ok());
+        assert!(result.unwrap().value.len() == 2);
+        assert_eq!(ctx.value[1].content, "This late pivot means we don't have time to boil the ocean for the client deliverable.");
+
+        let mut ctx = data();
+        let result = ai.shrink_conversation(&mut ctx, 48);
+        assert!(result.is_err());
+
+        let mut ctx = data();
+        let result = ai.shrink_conversation(&mut ctx, 71);
+        assert!(result.is_ok());
+        assert!(result.unwrap().value.len() == 3);
+        assert_eq!(
+            ctx.value[1].content,
+            "Let's talk later when we're less busy about how to do better."
+        );
     }
 }
